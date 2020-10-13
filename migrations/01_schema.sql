949ceed83cd2a88b73e5ae90c6baab1044f4d2c9
amarkham1 <andrewjmarkham1@gmail.com>
2020-10-13 23:50:21 +0000

    fix schema to align with solution

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,12 +1,12 @@
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS properties;
-DROP TABLE IF EXISTS reservations;
-DROP TABLE IF EXISTS property_reviews;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS properties CASCADE;
+DROP TABLE IF EXISTS reservations CASCADE;
+DROP TABLE IF EXISTS property_reviews CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  email_address VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
 );
 
@@ -18,16 +18,17 @@ CREATE TABLE properties (
   description TEXT,
   thumbnail_photo_url VARCHAR(255) NOT NULL,
   cover_photo_url VARCHAR(255) NOT NULL,
-  cost_per_night INTEGER NOT NULL DEFAULT 0,
-  parking_spaces INTEGER NOT NULL DEFAULT 0,
-  number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
-  number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
+  cost_per_night INTEGER  NOT NULL DEFAULT 0,
+  parking_spaces INTEGER  NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER  NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER  NOT NULL DEFAULT 0,
 
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
   city VARCHAR(255) NOT NULL,
   province VARCHAR(255) NOT NULL,
   post_code VARCHAR(255) NOT NULL,
+
   active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
